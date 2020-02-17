2f272bfe479b2d1b97290dbc6881f27143a30b28
syifaandini <syifandin@gmail.com>
2020-02-17 14:09:26 +0700

    Update load.js

--- load.js
+++ load.js
@@ -1,9 +1,9 @@
 // lOADER
 //SVG LOGO
-$('html, body').css({
-  overflow: 'hidden',
-  height: '100%'
-});
+// $('html, body').css({
+//   overflow: 'hidden',
+//   height: '100%'
+// });
 
 var bar = new ldBar("#myItem1",{
   "type": 'fill'
